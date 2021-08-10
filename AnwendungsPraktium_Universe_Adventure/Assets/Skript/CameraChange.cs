a2fabced5b9dd6380d6e7ddd394bb98bbec4021d
wael-nj <wael.najiba@outlook.com>
2021-08-10 18:29:51 +0200

    Projekt Hochladen

---
+++ AnwendungsPraktium_Universe_Adventure/Assets/Skript/CameraChange.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+
+
+    public GameObject thridCam;
+    public GameObject firstCam;
+    /*
+            CamMode Dient die  Modauswähle des Kamera
+
+    */
+    public int camMode;
+
+
+
+    void Start()
+    {
+
+    }
+
+    /*
+        Im InputManger ist eine Drucktaste definiert, mit der wir die Kamera auswählen können. 
+        
+    */
+    void Update()
+    {
+        if (Input.GetButton("Camera"))
+        {
+            if (camMode == 1)
+            {
+                camMode = 0;
+            }
+            else
+            {
+                camMode += 1;
+            }
+            StartCoroutine(camChange());
+        }
+    }
+
+
+    /*
+        Diese Methode wird verwendet, um zu bestimmen, welche Kamera aktiviert wird, wenn die Taste gedrückt wird  
+        wenn CamMode 0, wird die dritte Kamera aktiviert und die erste Kamera deaktiviert.
+        wenn CamMode 1,wird die erste Kamera  aktiviert und die dritte Kamera deaktiviert.
+   */
+    IEnumerator camChange()
+    {
+        yield return new WaitForSeconds(0.01f);
+        if (camMode == 0)
+        {
+            thridCam.SetActive(true);
+            firstCam.SetActive(false);
+        }
+        if (camMode == 1)
+        {
+            thridCam.SetActive(false);
+            firstCam.SetActive(true);
+        }
+
+    }
+}
---
+++ AnwendungsPraktium_Universe_Adventure/Assets/Skript/Enemy_AI.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AI : MonoBehaviour
+{
+
+
+    
+
+
+    /*
+
+    distance              
+    transformPlayer    aktulle Koordinaten des Spieler
+    checkDisatnce      wie viel ist die Abstand zwischen den Gegener und der Spieler
+    rotationspeed      Genger Roationsgeschwindigkeit 
+    moveEnemyspeed     Genger Bewegungsgeschwindigkeit zum Spieler 
+    trackZone          Abstand indem der Gegner die Zeit 
+    attackZone         Angriffe intravall Zeit 
+
+    */
+    [Header("Enemy AI Setting")]
+    [SerializeField] float distance;
+    [SerializeField] Transform transformPlayer;
+    [SerializeField] float checkDisatnce = 100f;
+
+    [SerializeField] float rotationspeed = 3.0f;
+    [SerializeField] float moveEnemyspeed = 10f;
+    [SerializeField] float trackZone = 50f;
+    [SerializeField] float attackZone = 30f;
+
+    [SerializeField] float lifeReduce = 20f;
+    [SerializeField] float attackInterval = 1f;
+
+    [SerializeField] GameObject[] lasers;
+  
+    private float attackTime;
+
+    /*
+         Bestimmung der aktuellen Position der Spieler 
+         Bestimmung der aktuellen Zeit bis zum Angriff. 
+
+    */
+    void Start()
+    {
+
+        transformPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+        attackTime = Time.time;
+
+    }
+
+
+   
+
+    /*
+  
+        die Entfernung zwischen den Spielern und den Gegnern wird berechnet
+
+        Solange der Spieler noch nicht vom Gegner entdeckt wurde, wird die Methode Check() aufgerufen
+        
+        Wenn der Spieler in die Zone des Gegners eindringt, wird die Methode Attack() aufgerufen, um den Angriff zu starten.
+
+         Der Spieler wird vom Gegner verfolgt, wenn er in der Verfolgungszone bleibt  
+
+    */
+    void Update()
+    {
+        distance = Vector3.Distance(transformPlayer.position, transform.position);
+        if (distance < checkDisatnce)
+        {
+            Check();
+           
+        }
+        if (distance < attackZone)
+        {
+            attack();
+            
+
+        }
+        else if (distance < trackZone)
+        {
+            trackTarget();
+             
+           
+        }
+    }
+
+    void Check()
+    {
+        /*
+            Look at Spieler 
+        */
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(transformPlayer.position - transform.position), rotationspeed * Time.deltaTime);
+    }
+
+    void trackTarget()
+    {
+
+         /*
+            Gegnerbewegung zu Spierler
+        */
+        transform.position += transform.forward * moveEnemyspeed * Time.deltaTime;
+        setlaserON(false);
+    }
+
+
+    /*
+       Beim Angriff wird der LaserEffact aktiviert und die GitHit-Methode aufgerufen, so dass das Leben des Spielers reduziert wird.
+       Außerdem wird der Angriff jede Sekunde durchgeführt
+
+
+    */
+    void attack()
+    {
+        setlaserON(true);
+
+        if (Time.time > attackTime)
+        {
+            transformPlayer.SendMessage("GetHit", lifeReduce, SendMessageOptions.DontRequireReceiver);
+            attackTime = Time.time + attackInterval;
+        }
+    }
+
+
+    /*
+        Laser aktiviert Method
+    */
+    void setlaserON(bool isActive)
+    {
+
+        foreach (GameObject item in lasers)
+        {
+
+            var emissionModule = item.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
+        }
+
+
+    }
+
+}
