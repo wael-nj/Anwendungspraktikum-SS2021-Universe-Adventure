929669922bd8162126fd3c7cabcbac8566bfef15
w96j0 <57408864+w96j0@users.noreply.github.com>
2021-08-12 12:27:47 +0200

    Kommentare ändern

--- AnwendungsPraktium_Universe_Adventure/Assets/Skript/GameOver.cs
+++ AnwendungsPraktium_Universe_Adventure/Assets/Skript/GameOver.cs
@@ -8,31 +8,17 @@ public class GameOver : MonoBehaviour
 {
 
     /*
-       Dieses Skript lädt die Spielszene für den Spieler, 
-       wenn er die Restarttaste drückt oder wenn er das Spiel beenden möchte, wenn er die Exittaste drückt
+       Dieses Skript funktioniert wie das Skript Handler, um Spielszene für den Spieler zu laden.
+       ???? wenn er die Restarttaste drückt oder wenn er das Spiel beenden möchte, wenn er die Exittaste drückt
 
      */
     public void Restart()
     {
 
-        /* 
-                MainMeauszene hat die Index 0
-                MainGameszene hat die Index 1
-                GameOverszene hat die Inex 2 
-
-
-                Wird die aktulle Szene  den Index - 1 geholt dann auf den Bildschirm geladen 
-                damit wir den Spieleszene nochmal aud den Bildschrim laden  
-           */
         int mainGameScene = SceneManager.GetActiveScene().buildIndex - 1;
         SceneManager.LoadScene(mainGameScene);
 
     }
-    /* 
-            Game Beenden
-
-
-       */
 
     public void exitGame()
     {
